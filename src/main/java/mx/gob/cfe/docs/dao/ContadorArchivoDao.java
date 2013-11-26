8151917f542ad078d813826025846ccd7615ad98
sam <samuel.9401@gmail.com>
2013-11-25 18:57:33 -0600

    Se agrego paquete de daos

---
+++ src/main/java/mx/gob/cfe/docs/dao/ContadorArchivoDao.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.gob.cfe.docs.dao;
+
+import mx.gob.cfe.documentos.model.ContadorArchivo;
+import mx.gob.cfe.documentos.model.Documento;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author develop
+ */
+@Repository
+@Transactional
+public class ContadorArchivoDao {
+
+    private static final Logger log = LoggerFactory.getLogger(DocumentoDao.class);
+    @Autowired
+    @Qualifier("sessionFactory")
+    private SessionFactory sessionFactory;
+
+    private Session currentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    public ContadorArchivo crea(ContadorArchivo contadorArchivo) {
+        currentSession().save(contadorArchivo);
+        return contadorArchivo;
+    }
+
+    public ContadorArchivo actualiza(ContadorArchivo contadorArchivo) {
+        currentSession().update(contadorArchivo);
+        currentSession().flush();
+        return contadorArchivo;
+    }
+
+    public ContadorArchivo obtiene(Long tipoArchivoId) {
+        ContadorArchivo contadorArchivo = (ContadorArchivo) currentSession().get(Documento.class, tipoArchivoId);
+        return contadorArchivo;
+    }
+
+    @Transactional(readOnly = true)
+    public ContadorArchivo obtiene(String documento) {
+        Query query = currentSession().createQuery(
+                "select c from ContadorArchivo c where c.documento = :documento");
+        query.setString("documento", documento);
+        return (ContadorArchivo) query.uniqueResult();
+    }
+
+}
---
+++ src/main/java/mx/gob/cfe/docs/dao/DocumentoDao.java
@@ -0,0 +1,69 @@
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates and open the template
+ * in the editor.
+ */
+package mx.gob.cfe.docs.dao;
+
+import java.util.Date;
+import java.util.List;
+import mx.gob.cfe.documentos.model.Documento;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author develop
+ */
+@Repository
+@Transactional
+public class DocumentoDao {
+
+    private static final Logger log = LoggerFactory.getLogger(DocumentoDao.class);
+    @Autowired
+    @Qualifier("sessionFactory")
+    private SessionFactory sessionFactory;
+
+    private Session currentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    public Documento crea(Documento documento) {
+        documento.setFecha(new Date());
+        currentSession().save(documento);
+        return documento;
+    }
+
+    public Documento actualiza(Documento documento) {
+        documento.setFecha(new Date());
+        currentSession().update(documento);
+        currentSession().flush();
+        return documento;
+    }
+
+    public String elimina(Long documentoId) {
+        Documento documento = (Documento) currentSession().get(Documento.class, documentoId);
+        String autor = documento.getAutor();
+        currentSession().delete(documento);
+        currentSession().flush();
+        return autor;
+    }
+
+    public Documento obtiene(Long documentoId) {
+        Documento articulo = (Documento) currentSession().get(Documento.class, documentoId);
+        return articulo;
+    }
+
+    public List<Documento> lista(String tipoArchivo) {
+        Query query = currentSession().createQuery("select a from Documento a where a.tipoDocumento =:tipoArchivo order by fecha desc");
+        query.setString("tipoArchivo", tipoArchivo);
+        return query.list();
+    }
+}
